d7b510689fe7de52b459a192c636fa5bc723566a
RaulJimenezOrtega <hhkaos@gmail.com>
2017-06-20 12:45:17 +0200

    Addin devsummit design

--- js/docs.js
+++ js/docs.js
@@ -2,11 +2,16 @@ $(function() {
     // Code for docs demos
     function createColorpickers() {
         // Api demo
-        
+
 
         $('.demo-auto').colorpicker().on('changeColor.colorpicker', function(event){
             var part = $(this).data("part"),
                 color = event.color.toHex();
+// debugger
+            //if($("."+part))
+            if($("#"+part).val() === ''){
+              color = 'rgba(0,0,0,0)';
+            }
 
             if(part == "bgHeader" || part == "bgFooter"){
                 $("."+part).css("background-color", color);
@@ -24,10 +29,15 @@ $(function() {
         $(".logo").attr("src",$(this).val());
         console.log($(this).val());
     });
+
+    $("#bgSrc").change(function(){
+      $(".table-container").css("background-image","url("+$(this).val()+")");
+    });
+
     $("#pdf").click(function(){
         var params = $("form").serialize().replace(/%23/g, "");
         console.log(params);
-        
+
         $(this).attr("href","bagde-generator.html?"+params);
     });
     $("#width").change(function(){
